2cc548dab6878238152ee8d0bd7b989baea9d70a
Chukwudumebi <chukwudumebiughonu@gmail.com>
2025-07-19 13:33:00 +0100

    01 setput

---
+++ spawn/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <Button variant="secondary">
+        Click me
+      </Button>
+    </div>
+  )
+}
+
+export default Page;
